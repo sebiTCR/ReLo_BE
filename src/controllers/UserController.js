00ccd526ae21cd5c3341ca82fd2b3dba8509d639
sebiTCR <sebitcr@pop-os.localdomain>
2020-11-21 17:20:30 +0200

    Fixed MA-SU issue for the updateAccount method

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -76,25 +76,41 @@ module.exports.getAccount = (req,res) =>
 module.exports.updateAccount = (req,res) =>
 {
 
-    user.findById(req.params.uname, (err,usr)=>
-    {        
-        usr.username = req.body.username ? req.body.username : pers.username;
-        usr.password = req.body.password ? req.body.password : pers.password;
-        
-        usr.save((err)=>
+    user.findOne({username: req.body.username}, (err, usrChk)=>
+    {
+        if(!err)
         {
-            if(err)
+            if(usrChk)
             {
-                res.json({
-                    message: err,
+                res.json({message: "Username already in use!"})
+            }
+            else
+            {
+                user.findById(req.params.uname, (err,usr)=>
+                {        
+                    usr.username = req.body.username ? req.body.username : usr.username;
+                    usr.password = req.body.password ? req.body.password : usr.password;
+                    
+                    usr.save((err)=>
+                    {
+                        if(err)
+                        {
+                            res.json({
+                                message: err,
+                            });
+                        }
+                    });
+                    res.json({
+                        message: "User updated"
+                    })
                 });
             }
-        });
-        res.json({
-            message: "User updated"
-        })
+        }
+        else
+        {
+            res.json({message: err})
+        }
     });
-    
 }
 
 
