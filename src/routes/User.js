ee93d3f4d42c523dc0b99f4b368e7ebae84728b0
sebiTCR <17337551+sebiTCR@users.noreply.github.com>
2020-11-21 23:07:25 +0200

    Revert " ➕ Added a basic login method"

--- src/routes/User.js
+++ src/routes/User.js
@@ -5,13 +5,11 @@ const userController = require('../controllers/UserController')
     Router.route('/accounts')
           .get(userController.getAccounts)
 
-    Router.route('/account/:uname')
-          .get(userController.getAccount)
-          .post(userController.createUser)
-          .put(userController.updateAccount)
-          .delete(userController.deleteAccount)
+      Router.route('/account/:uname')
+            .get(userController.getAccount)
+            .post(userController.createUser)
+            .put(userController.updateAccount)
+            .delete(userController.deleteAccount)
 
-    Router.route('/login/')
-          .post(userController.loginUser)
 
 module.exports = Router;
